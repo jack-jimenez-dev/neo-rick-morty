fe996e841c1d02f295fa82fff4b07af9dbbe78de
jack_z <jackzhj34@gmail.com>
2025-04-11 21:15:37 -0500

    feat: Agregamos estilos a las tarjetas

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 //Mostrar los personajes sin filtrar
 // Obtener todos los personajes llamando a la API
 //Renderizarlos en nuesrto dom
+        /* En JavaScript, renderizar en nuestro DOM se refiere al proceso de mostrar 
+        o actualizar elementos visuales en la página web (es decir, en el DOM, que 
+        es la representación en memoria de la estructura de la página).
+        */
 //cuando cambian los filtros se tiene que hacer un neuvo llamado a la API
 //Se debe renderizar neuvamente
 
@@ -31,18 +35,20 @@ async function getCharacters(name, status) {
 async function displayCharacters(name,status){
     
     //Obtener personajes filtrados
-    const characters = await getCharacters(name, status);
+    const characters = await getCharacters(name, status);//Es la funcion que llama a la api
 
     //renderizar los personajes
     for( let character of characters){
         const card = document.createElement('div');
         card.classList.add('character-card');
+        
         card.innerHTML=  ` 
         <img src="${character.image}" />
         <h2> ${character.name} </h2>
         <p> Status: ${character.status} </p>
         <p> Especie: ${character.species} </p>
         `;
+
         characterE1.appendChild(card);
     }  
 }
@@ -50,3 +56,7 @@ async function displayCharacters(name,status){
 
 
 displayCharacters();
+
+
+//Creamos evento:
+ //nameFilterE1.addEventListener
